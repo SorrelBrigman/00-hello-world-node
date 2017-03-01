20b20600f92896dbec1e09a81ebe48253b433f1e
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-01 13:21:29 -0600

    added comment in js file explaining what user should see

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,4 @@
 
 //call this function in the terminal through the command "node helloWorld.js"
 console.log("Hello World!");
+//you should see "Hello World" in your terminal after running this command.
